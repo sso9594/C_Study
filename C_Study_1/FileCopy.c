31ddb07d4872d9ee6d36e116ba16a093deef5560
신승용 <sinseung-yong@sinseung-yong-ui-MacBookAir.local>
2023-04-10 10:24:23 +0900

    update

--- C_Study_1/FileCopy.c
+++ C_Study_1/FileCopy.c
@@ -7,7 +7,16 @@ int main() {
     char c;
 
     fp1 = fopen("a.txt", "r");
+    if(fp1==NULL){
+        printf("file을 열지 못했습니다.\n");
+        return 1;
+    }
+
     fp2 = fopen("b.txt", "w");
+    if(fp2==NULL){
+        printf("file을 열지 못했습니다.\n");
+        return 1;
+    }
 
     while ((c = fgetc(fp1)) != EOF) {
         fputc(c, fp2);
@@ -17,6 +26,10 @@ int main() {
     fclose(fp2);
 
     fp2 = fopen("b.txt", "r");
+    if(fp2==NULL){
+        printf("file을 열지 못했습니다.\n");
+        return 1;
+    }
 
     char buffer[100];
     while (fgets(buffer, sizeof(buffer), fp2) != NULL) {
